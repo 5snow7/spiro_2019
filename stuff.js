bf8c812c5933d5f2c4942b66eec2bb8e75c3ff66
Mark Womack <mwomack@pcoe.k12.ca.us>
2019-10-19 17:19:37 -0700

    initial commit

---
+++ stuff.js
@@ -0,0 +1,23 @@
+function reStart(){
+ alert("taking a break?"); setup();
+}
+
+function length1(){
+ val1=document.getElementById("ran1").valueAsNumber;
+}
+
+function length2(){
+ val2=document.getElementById("ran2").valueAsNumber;
+}
+
+function length3(){
+ val3=document.getElementById("ran3").valueAsNumber;
+}
+
+function weigths(){
+  let str=document.getElementById("inp1");
+  let strAr=[];
+  strAr=str.split(',');
+  w1a=strAr[0];w2a=strAr[1];w3a=strAr[2];
+  
+}
